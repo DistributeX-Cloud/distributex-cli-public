132ff8d71da1822f14fba5cd8d645714618a8f1a
DistributeX <2zsocialmediamail@gmail.com>
2025-12-03 14:21:47 -0500

    Update __init__.py

--- python/distributex/__init__.py
+++ python/distributex/__init__.py
@@ -3,19 +3,18 @@ DistributeX Python SDK
 ======================
 Distributed computing platform for running code on a global pool of resources.
 
-Installation:
-    pip install distributex
-
-Usage:
+Quick Start:
     from distributex import DistributeX
     
     dx = DistributeX(api_key="your_api_key")
     result = dx.run(my_function, args=(data,), workers=4, gpu=True)
 
+Documentation:
+    https://distributex-cloud-network.pages.dev/
 """
 
-from .client import DistributeX, init, run, run_script, run_docker
+from .client import DistributeX, Task
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __author__ = "DistributeX Team"
-__all__ = ["DistributeX", "init", "run", "run_script", "run_docker"]
+__all__ = ["DistributeX", "Task"]
